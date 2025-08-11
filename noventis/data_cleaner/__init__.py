15e71e45b2c86af663727b07b3e159631b61914f
orieabynmlna <orieabyanm@gmail.com>
2025-08-11 21:28:06 +0700

    menggabungkan kode da n ds

--- noventis/data_cleaner/__init__.py
+++ noventis/data_cleaner/__init__.py
@@ -1,8 +1,12 @@
 from .scaling import NoventisScaler
 from .encoding import NoventisEncoder
+from .imputing import NoventisImputer
+from .outlier_handling import NoventisOutlierHandler
 # from .null_handling import YourNullHandlerClass
 
 __all__ = [
     'NoventisScaler',
-    'NoventisEncoder'  
+    'NoventisEncoder',
+    'NoventisImputer',
+    'NoventisOutlierHandler',
 ]
