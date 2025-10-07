16c20eb267e0128ca92f012b40dad0dd44f98dc7
orieabynmlna <orieabyanm@gmail.com>
2025-10-07 21:57:07 +0700

    fix awalan

--- noventis/__init__.py
+++ noventis/__init__.py
@@ -1,5 +1,6 @@
 __version__ = "0.1.0"
 
+from .eda_auto.eda_auto import NoventisAutoEDA
 
 from .data_cleaner.auto import data_cleaner
 from .data_cleaner.encoding import NoventisEncoder
@@ -7,21 +8,16 @@ from .data_cleaner.scaling import NoventisScaler
 from .data_cleaner.imputing import NoventisImputer
 from .data_cleaner.outlier_handling import NoventisOutlierHandler
 
-
-from .eda_auto.eda_auto import NoventisAutoEDA
-
-
 from .predictor.manual import NoventisManualPredictor
 from .predictor.auto import NoventisAutoML
 
 __all__ = [
+    "NoventisAutoEDA",
     "data_cleaner",
     "NoventisEncoder",
     "NoventisScaler",
     "NoventisImputer",
     "NoventisOutlierHandler",
-    "NoventisAutoEDA",
     "NoventisManualPredictor",  
     "NoventisAutoML",
 ]
-
--- noventis/data_cleaner/__init__.py
+++ noventis/data_cleaner/__init__.py
@@ -2,7 +2,6 @@ from .scaling import NoventisScaler
 from .encoding import NoventisEncoder
 from .imputing import NoventisImputer
 from .outlier_handling import NoventisOutlierHandler
-# from .null_handling import YourNullHandlerClass
 from .auto import NoventisDataCleaner
 
 __all__ = [
